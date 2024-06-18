15a8ad6cc81692745196657bf23a021737ad681a
Husnain Asghar <husnainasghar67@gmail.com>
2024-06-18 21:32:08 +0200

    -Results -improvement

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block" v-if="showBlock">Click me</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click me</div>
 </template>
 
 <script>
@@ -8,20 +8,26 @@ export default {
   data() {
     return {
       showBlock: false,
+      timer: null,
+      reactionTime: 0,
     };
   },
   mounted() {
-    console.log("component mounted");
     setTimeout(() => {
       this.showBlock = true;
-      console.log(this.delay);
+      this.startTimer();
     }, this.delay);
   },
-  updated() {
-    console.log("updated");
-  },
-  unmounted() {
-    console.log("unmounted");
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit("end", this.reactionTime);
+    },
   },
 };
 </script>
